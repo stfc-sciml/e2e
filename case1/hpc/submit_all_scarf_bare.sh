48efb24e144f44128b56040706a13bfd035c937b
Samuel Jackson <samueljackson@outlook.com>
2021-08-18 10:11:18 +0100

    Update script

--- case1/hpc/submit_all_scarf_bare.sh
+++ case1/hpc/submit_all_scarf_bare.sh
@@ -3,9 +3,9 @@
 JOB_SCRIPT=hpc/submit_scarf_10338_bare.sh
 
 # Each node has 24 cores. We need an odd number of tasks, so we use 23 here.
-sbatch --nodes=1 --ntasks=23 RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_class2d_0.sh $JOB_SCRIPT
-sbatch --nodes=1 --ntasks=23 RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_class3d_1.sh $JOB_SCRIPT
-sbatch --nodes=1 --ntasks=23 RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_refine3d_2.sh $JOB_SCRIPT
-sbatch --nodes=1 --ntasks=23 RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_refine3d_3.sh $JOB_SCRIPT
-sbatch --nodes=1 --ntasks=23 RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_refine3d_4.sh $JOB_SCRIPT
-sbatch --nodes=1 --ntasks=23 RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_polish_5.sh $JOB_SCRIPT
+RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_class2d_0.sh sbatch --nodes=1 --ntasks=23  $JOB_SCRIPT
+RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_class3d_1.sh sbatch --nodes=1 --ntasks=23  $JOB_SCRIPT
+RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_refine3d_2.sh sbatch --nodes=1 --ntasks=23  $JOB_SCRIPT
+RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_refine3d_3.sh sbatch --nodes=1 --ntasks=23  $JOB_SCRIPT
+RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_refine3d_4.sh sbatch --nodes=1 --ntasks=23  $JOB_SCRIPT
+RELION_SCRIPT_NAME=./benchmark_scripts/10338/pipeline_polish_5.sh sbatch --nodes=1 --ntasks=23  $JOB_SCRIPT
