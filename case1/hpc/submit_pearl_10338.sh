83d2b0a1289691e6dc17957cd17191de3031464f
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2021-09-15 12:17:24 +0100

    Update PEARL submission script

--- case1/hpc/submit_pearl_10338.sh
+++ case1/hpc/submit_pearl_10338.sh
@@ -23,4 +23,7 @@ export RELION_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 export RELION_OPT_FLAGS='--gpu --dont_combine_weights_via_disc --pool 30'
 
 # Run pipeline
-./benchmark_scripts/benchmark_relion.py ./benchmark_scripts/10338/pipeline_polish_5.sh
+#./benchmark_scripts/benchmark_relion.py ./benchmark_scripts/10338/pipeline_refine3d_4.sh
+#./benchmark_scripts/benchmark_relion.py ./benchmark_scripts/10338/pipeline_polish_5.sh
+./benchmark_scripts/benchmark_relion.py ./benchmark_scripts/10338/pipeline_class3d_1.sh
+#./benchmark_scripts/benchmark_relion.py ./benchmark_scripts/10338/pipeline_class2d_0.sh
